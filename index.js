4c0e3a6943e9b95aee270e2ac3f470f35682a035
almamuntmn <almamuncool88@gmail.com>
2025-12-09 22:46:50 +0600

    added sign in

--- index.js
+++ index.js
@@ -73,13 +73,28 @@ async function run() {
       res.send(result);
     })
     
-
     app.get('/users', async(req, res)=>{
       const result = await usersCollection.find().toArray();
       res.send(result);
     })
+    
+    app.patch('/users', async(req, res)=>{
+      const signInInfo = req.body;
+      const email = signInInfo.email;
+      const filter = {email: email};
+      const updatedDoc = {
+        $set: signInInfo
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
 
-
+    app.delete('/users/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
